8a9eeeac57774371f870c5a38d0bd35e49b314e7
Sousa Gaspar <sousadgaspar@gmail.com>
2021-04-11 14:35:14 +0100

    added a resourcefull Tenant controller

--- src/controllers/TenantController.ts
+++ src/controllers/TenantController.ts
@@ -3,6 +3,9 @@ import {Request, Response} from 'express';
 import {Tenant} from '../entity/Tenant';
 import {validationResult} from 'express-validator';
 
+//tenant instance for all functions
+const tenant = new Tenant();
+
 export async function create (request: Request, response: Response) {
 
     //Validate the request
@@ -12,7 +15,6 @@ export async function create (request: Request, response: Response) {
     }
 
     //populate the tenant object with the request payload
-    const tenant = new Tenant();
     tenant.name = request.body.name;
     tenant.description = request.body.description;
     tenant.currency = request.body.currency;
@@ -34,5 +36,116 @@ export async function create (request: Request, response: Response) {
                 sqlMessage: error.sqlMessage,
             });
         });
+}
 
+
+export async function index (request: Request, response: Response) {
+
+    const tenantRepository = getRepository(Tenant);
+    await tenantRepository.find(tenant)
+        .then(foundTenants => {
+            console.log(":::::::::: FOUND TENANTS ::::::::::::::    " + JSON.stringify(foundTenants));
+            response.status(200).send(foundTenants);
+        })
+        .catch(error => {
+            response.status(500).send({
+                errorName: error.name,
+                errorMessage: error.message,
+                errorNumber: error.errno,
+                errorCode: error.code,
+                sqlMessage: error.sqlMessage,
+            });
+        });
+}
+
+
+export async function show (request: Request, response: Response) {
+
+    const tenantRepository = getRepository(Tenant);
+    tenantRepository.findOne({id: request.params.id})
+        .then(foundTenant => {
+            console.log(":::::::::::    FOUND TENANT    ::::::::::::::: " + JSON.stringify(foundTenant))
+            response.status(200).send(foundTenant);
+        })
+        .catch(error => {
+            console.log(":::::::::::    ERROR FINDING TENANT    ::::::::::::::: " + JSON.stringify(error))
+            response.status(500).send({
+                errorName: error.name,
+                errorMessage: error.message,
+                errorNumber: error.errno,
+                errorCode: error.code,
+                sqlMessage: error.sqlMessage,
+            });
+        });
+}
+
+
+export async function update (request: Request, response: Response) {
+
+    const tenantRepository = getRepository(Tenant);
+    const foundTenant = await tenantRepository.findOne({id: request.params.id});
+    if(request.body.name) foundTenant.name = request.body.name;
+    if(request.body.description) foundTenant.description = request.body.description;
+    if(request.body.currency) foundTenant.currency = request.body.currency;
+    if(request.body.paymentGateways) foundTenant.paymentGateways = request.body.paymentGateways;
+
+    await tenantRepository.save(foundTenant)
+                .then(savedTenant => {
+                    console.log(":::::::::::    SAVING TENANT    ::::::::::::::: " + JSON.stringify(savedTenant));
+                    return response.status(200).send(savedTenant);
+                })
+                .catch(error => {
+
+                    console.log(":::::::::::    ERROR SAVING UPDATED TENANT    ::::::::::::::: " + JSON.stringify(error));
+
+                    return response.status(500).send({
+                        errorName: error.name,
+                        errorMessage: error.message,
+                        errorNumber: error.errno,
+                        errorCode: error.code,
+                        sqlMessage: error.sqlMessage,
+                    });
+                });
+}
+
+
+export async function del(request: Request, response: Response) {
+
+    const tenantRepository = getRepository(Tenant);
+        tenantRepository.delete({id: request.params.id})
+            .then(affectedRows => {
+                console.log(":::::::::::    DELETING TENANT    ::::::::::::::: " + JSON.stringify(affectedRows));
+                return response.status(200).send(affectedRows);
+            })
+            .catch(error => {
+                console.log(":::::::::::    ERROR DELETING TENANT    ::::::::::::::: " + JSON.stringify(error));
+                return response.status(500).send({
+                    errorName: error.name,
+                    errorMessage: error.message,
+                    errorNumber: error.errno,
+                    errorCode: error.code,
+                    sqlMessage: error.sqlMessage,
+                });
+            });
+}
+
+
+export async function softDelete(request: Request, response: Response) {
+
+    const tenantRepository = getRepository(Tenant);
+        tenantRepository.softDelete({id: request.params.id})
+            .then(affectedRows => {
+                console.log(":::::::::::    DELETING TENANT    ::::::::::::::: " + JSON.stringify(affectedRows));
+                return response.status(200).send(affectedRows);
+            })
+            .catch(error => {
+                console.log(":::::::::::    ERROR DELETING TENANT    ::::::::::::::: " + JSON.stringify(error));
+                return response.status(500).send({
+                    errorName: error.name,
+                    errorMessage: error.message,
+                    errorNumber: error.errno,
+                    errorCode: error.code,
+                    sqlMessage: error.sqlMessage,
+                });
+            });
 }
--- src/routes/tenant.js
+++ src/routes/tenant.js
@@ -5,6 +5,11 @@ const tenantRouter = require('express').Router();
 
 
 tenantRouter.post('/api/tenant', tenantValidator.validate('create'), tenantController.create);
+tenantRouter.get('/api/tenants', tenantController.index);
+tenantRouter.get('/api/tenant/:id', tenantValidator.validate('show'), tenantController.show);
+tenantRouter.put('/api/tenant/:id', tenantController.update);
+tenantRouter.delete('/api/tenant/:id', tenantValidator.validate('softDelete'), tenantController.softDelete);
+tenantRouter.delete('/api/tenant/:id/delete', tenantValidator.validate('delete'), tenantController.del);
 
 
 module.exports.tenantRoutes = tenantRouter;
